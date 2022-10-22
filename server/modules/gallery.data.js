b2f7e31b9e54fe353d9b6b0efa3cba1cfb751e9b
Astrid Pulse <astridcpulse@gmail.com>
2022-10-22 13:24:36 -0500

    initial commit. Add photos to gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/smiling_face.jpg', description: 'Photo of Astrid. Happy, for she is beautiful.', likes: 0 },
+    { id: 3, path: 'images/serious_face.jpg', description: 'Photo of Astrid. Beautiful and dignified.', likes: 0 }
 ];
 
 module.exports = galleryItems;
